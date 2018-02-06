4a223885340e694f40e7a046f728fb95feb8757a
loicbourgois <loic@bourgois.pro>
2018-02-06 12:52:02 +0100

    Generate genre.tsv and movie-genre.tsv

--- generate-tsv.js
+++ generate-tsv.js
@@ -10,13 +10,21 @@ const date = config.date;
 const folderPath = `/home/user/esploristo-imdb/data/imdb/documents/v1/${date}`;
 const fileName = `${folderPath}/title.basics.tsv`;
 const movieTsvFile = `${folderPath}/movie.tsv`;
+const genreTsvFile = `${folderPath}/genre.tsv`;
+const movieGenreTsvFile = `${folderPath}/movie-genre.tsv`;
 const types = config.types;
+const genres = config.genres;
 // const genresStrById = config.genresStrById;
 const genresStrUpById = config.genresStrUpById;
 const separator = '\t';
 const s = separator;
 const warningEnabled = false;
 const testEnabled = false;
+const headers = {
+	movie: 'imdb_id\ttype\toriginal_title\tprimary_title\tstart_year\tend_year\truntime\tgenres',
+	genre: 'id\tid_str\tid_str_up',
+	movieGenre: 'imdb_id\tgenre_id'
+};
 
 const logError = function(message, line) {
 	console.error(chalk.red(`Line ${line} : ${message}`));
@@ -41,18 +49,36 @@ const generateTsv = function () {
 	}
 };
 
-const generateTsv2 = function() {
+const generateTsv2 = function () {
+	if(fs.existsSync(movieGenreTsvFile)) {
+		fs.unlink(movieGenreTsvFile, function(error) {
+			if(error) {
+				logError(error);
+			} else {
+				generateTsv3();
+			}
+		});
+	} else {
+		generateTsv3();
+	}
+};
+
+const generateTsv3 = function() {
 	exec(`wc -l ${fileName}`, function (error, results) {
 		if(error) {
 			return logError(error);
 		} else {
-			generateTsv3(parseInt(results));
+			generateTsv4(parseInt(results));
 		}
 	});
 };
 
-const generateTsv3 = function (linesCount) {
+const generateTsv4 = function (linesCount) {
+	writeGenreTsvFile();
 	let stream = fs.createWriteStream(movieTsvFile, {flags:'a'});
+	let streamMovieGenre = fs.createWriteStream(movieGenreTsvFile, {flags:'a'});
+	stream.write(`${headers.movie}\n`);
+	streamMovieGenre.write(`${headers.movieGenre}\n`);
 	let i = 0;
 	let lr = new LineByLineReader(fileName);
 	lr.on('line', function (line) {
@@ -84,7 +110,7 @@ const generateTsv3 = function (linesCount) {
 		lineGenres = lineGenres.split(',');
 		lineGenres = lineGenres.filter(function(genreStrUp) {
 			if(genreStrUp === '\\N') {
-				logWarning(`No genre`, i);
+				logWarning('No genre', i);
 				return false;
 			}
 			let b = genresStrUpById.includes(genreStrUp);
@@ -92,6 +118,9 @@ const generateTsv3 = function (linesCount) {
 				logError(`Genre should not be ${genreStrUp}`, i);
 			}
 			return b;
+		});
+		lineGenres.forEach(function(genreStrUp) {
+			streamMovieGenre.write(`${imdbId}${s}${genres[genreStrUp].id}\n`);
 		});
 		lineGenres = lineGenres.length ? lineGenres : null;
 		line = `${imdbId}${s}${type}${s}${originalTitle}${s}${primaryTitle}${s}${startYear}${s}${endYear}${s}${runtime}${s}${lineGenres}`;
@@ -103,16 +132,38 @@ const generateTsv3 = function (linesCount) {
 			if(error) {
 				return logError(error);
 			} else {
-				console.log(`File in : ${results}`);
-				exec(`wc -l ${movieTsvFile}`, function (error, results) {
+				console.log(`File in :\n${results}`);
+				exec(`wc -l ${genreTsvFile} ${movieTsvFile} ${movieGenreTsvFile}`, function (error, results) {
 					if(error) {
 						return logError(error);
 					} else {
-						console.log(`File out : ${results}`);
+						console.log(`Files out :\n${results}`);
 					}
 				});
 			}
 		});
+	});
+};
+
+const writeGenreTsvFile = function() {
+	if(fs.existsSync(genreTsvFile)) {
+		fs.unlink(genreTsvFile, function(error) {
+			if(error) {
+				logError(error);
+			} else {
+				writeGenreTsvFile2();
+			}
+		});
+	} else {
+		writeGenreTsvFile2();
+	}
+};
+
+const writeGenreTsvFile2 = function() {
+	let stream = fs.createWriteStream(genreTsvFile, {flags:'a'});
+	stream.write(`${headers.genre}\n`);
+	config.genres.forEach(function(genre) {
+		stream.write(`${genre.id}${s}${genre.idStr}${s}${genre.idStrUp}\n`);
 	});
 };
 
